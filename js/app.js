2b621136332a3cd1eb95de70c9914e69e71a32c4
HurtaEdu <33555011+HurtaEdu@users.noreply.github.com>
2017-12-18 15:01:45 -0500

    Comentarios
    Colocamos comentarios en el js para tener una referencia en donde iria en js de cada html

---
+++ js/app.js
@@ -0,0 +1,58 @@
+/* Index.html */
+
+
+
+
+
+
+
+
+
+/* Cursos.html */
+
+
+
+
+
+
+
+
+/* Proyectos.html */
+
+
+
+
+
+
+
+
+
+/* Acerca-de.html */
+
+
+
+
+
+
+
+
+
+/* Login.html */
+
+
+
+
+
+
+
+
+
+/* Register.html */
+
+
+
+
+
+
+
+
